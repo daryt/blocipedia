fda37e2626ef9769848011678de97eaf9d0d4a52
Tyler Dary <daryt@msn.com>
2014-10-07 14:40:14 +0000

    added static index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Blocipedia::Application.routes.draw do
+  get "static/index"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'static#index'
 
 
   # Example of regular route:
