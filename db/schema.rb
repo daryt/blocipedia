42011fc5ce775c5894151f56108226e8915962fa
Tyler Dary <daryt@msn.com>
2014-10-10 15:01:00 +0000

    created wikis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007171831) do
+ActiveRecord::Schema.define(version: 20141007172905) do
 
   create_table "pages", force: true do |t|
     t.string   "title"
@@ -42,5 +42,13 @@ ActiveRecord::Schema.define(version: 20141007171831) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "wikis", force: true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.boolean  "is_private"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
