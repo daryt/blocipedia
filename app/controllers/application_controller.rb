5eb1e9ae5844df59cdd03b388eed5bc67bceaf5c
Tyler Dary <daryt@msn.com>
2014-11-18 15:51:00 +0000

    added user name to registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError do |exception|
     redirect_to root_url, alert: exception.message
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+ 
+   def configure_permitted_parameters
+     devise_parameter_sanitizer.for(:sign_up) << :name
+   end
+
 end
